14cc06a9e845b863a87ae3a8ec2bf474b63d058c
dhivagarinfographicanalytics <dhivagar@infographicanalytics.com>
2024-01-06 09:54:45 +0530

    slider bug

--- src/Components/contentarea/RecentCourse.js
+++ src/Components/contentarea/RecentCourse.js
@@ -108,7 +108,7 @@ const truncateText = (text, maxLength) => {
         <Slider {...settings} className='mart20 ' >           
             {recentCourse?.map((course) => (
                     <div title={course.course_name} className="padr10 padl10 outline_remove slide-item " key={course?.subcategory_id} style={{borderRadius:"20px"}}>
-                        <Image src={course?.course_image || NoImg}  className=' border' style={{width:"410px", height:"auto", borderTopLeftRadius:"20px",borderTopRightRadius:"20px"}}/>
+                        <Image src={course?.course_image || NoImg}  className=' border' style={{width:"410px", height:"225px", borderTopLeftRadius:"20px",borderTopRightRadius:"20px"}}/>
                         <div className='padl10 border padb20 padr10 content-container' style={{width:"410px", height:"auto", borderBottomLeftRadius:"20px",borderBottomRightRadius:"20px"}}>
                             <p className="light_black" style={{ padding: "5px 0px 0 0px", fontSize: "14px" }}>Created by <b>{course.first_name}</b> on{" "}
                              {course.formatted_created_date}
