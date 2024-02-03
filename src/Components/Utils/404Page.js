3879009798d9adcde8a63edbce3c382204570324
dhivagarinfographicanalytics <dhivagar@infographicanalytics.com>
2024-02-03 13:00:14 +0530

    404 correction

--- src/Components/Utils/404Page.js
+++ src/Components/Utils/404Page.js
@@ -14,8 +14,8 @@ const errorpage = () => {
         <Link to="/"> <Button className='white fr dark_purple_bg fw600 fz20 ' style={{padding:"10px 70px", border:"none", borderRadius:"0", marginTop:"40px"}}>Go to Home </Button></Link>
        
      </Col>
-        <Col xs={12} sm={12} md={12} lg={7} xxl={8} className='vmiddle'>
-            <Image className='w80' src={IMG}></Image>
+        <Col xs={12} sm={12} md={12} lg={7} xxl={8} className='vmiddle error-img'>
+            <Image className='w80 constant-tilt-shake' src={IMG}></Image>
         </Col>
        
       
